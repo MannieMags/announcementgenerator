a875331a857e45fd462cb309c3cffe52312e540c
MannieMags <nkosiemmanuel0@gmail.com>
2026-04-07 13:29:53 +0200

    Use json.dumps() to safely encode API key in config.js

--- write_config.py
+++ write_config.py
@@ -1,4 +1,5 @@
 import os
+import json
 key = os.environ.get('PANGEA_API_KEY', '')
 with open('config.js', 'w') as f:
-    f.write('window.PANGEA_CONFIG = { apiKey: "' + key + '" };\n')
+    f.write('window.PANGEA_CONFIG = { apiKey: ' + json.dumps(key) + ' };\n')
